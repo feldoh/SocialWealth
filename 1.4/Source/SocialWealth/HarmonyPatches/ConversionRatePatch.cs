db57e6662bcfede3937b242ebcd9910db21c5f6f
Dexter Lowe <feldoh10@gmail.com>
2024-01-26 03:58:42 +0000

    Working POC

---
+++ 1.4/Source/SocialWealth/HarmonyPatches/ConversionRatePatch.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace SocialWealth.HarmonyPatches;
+
+[HarmonyPatch(typeof(CompAbilityEffect_Convert))]
+[HarmonyPatch("ExtraLabelMouseAttachment")]
+public static class ExtraLabelMouseAttachmentTranspiler
+{
+    public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+    {
+        List<CodeInstruction> codes = [..instructions];
+        List<CodeInstruction> newCodes = [];
+        bool found = false;
+        foreach (CodeInstruction t in codes)
+        {
+            switch (found)
+            {
+                case false when t.Calls(AccessTools.Method(typeof(GenText), nameof(GenText.ToStringPercent), new[]
+                {
+                    typeof(float)
+                })):
+                    found = true;
+                    newCodes.Add(new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(ExtraLabelMouseAttachmentTranspiler), nameof(GetWealthAdjustedCertaintyReduction))));
+                    break;
+                case true when t.opcode == OpCodes.Ret:
+                    newCodes.Add(new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(ExtraLabelMouseAttachmentTranspiler), nameof(AppendWealthAdjustedCertaintyReduction))));
+                    newCodes.Add(new CodeInstruction(OpCodes.Ret));
+                    break;
+                default:
+                    newCodes.Add(t);
+                    break;
+            }
+        }
+
+        return newCodes.AsEnumerable();
+    }
+
+    public static string GetWealthAdjustedCertaintyReduction(float originalValue)
+    {
+        // We know this is an attempt to reduce certainty, and they must be not our ideo so we always want to multiply
+        float certaintyChangeMultiplier = originalValue * GetCertaintyChangeMultiplier();
+        return certaintyChangeMultiplier.ToStringPercent();
+    }
+
+    public static float GetCertaintyChangeMultiplier()
+    {
+        return Find.CurrentMap.wealthWatcher.WealthTotal / SocialWealthMod.settings.neutralWealth;
+    }
+
+    public static string AppendWealthAdjustedCertaintyReduction(string s)
+    {
+        return $"{s}\n -  " + "SocialWealth_WealthConversionFactorDesc".Translate() + ": " +
+               GetCertaintyChangeMultiplier().ToStringPercent();
+    }
+}
+
+[HarmonyPatch(typeof(Pawn_IdeoTracker), nameof(Pawn_IdeoTracker.Certainty), MethodType.Setter)]
+public static class CertaintyPrefix
+{
+    public static bool Prefix(Pawn_IdeoTracker __instance, ref float value, Pawn ___pawn)
+    {
+        float currentCertainty = __instance.Certainty;
+        float certaintyDiff = currentCertainty - value;
+        if (!___pawn.Spawned || Math.Abs(certaintyDiff) < SocialWealthMod.settings.changeThreshold) return true;
+
+        // Calculate the scaling factor based on the colony's wealth
+        float scalingFactor = ExtraLabelMouseAttachmentTranspiler.GetCertaintyChangeMultiplier();
+        Verse.Log.Message($"Scaling factor: {scalingFactor}. Applying to {currentCertainty} -> {value}. Diff: {certaintyDiff}");
+
+        float finalCertaintyDiff;
+        // If the pawn's ideo is not the player's ideo
+        if (__instance.Ideo != Faction.OfPlayer.ideos.PrimaryIdeo)
+        {
+            finalCertaintyDiff = (value < currentCertainty
+                ? certaintyDiff * scalingFactor
+                : certaintyDiff / scalingFactor);
+        }
+        else
+        {
+            finalCertaintyDiff = (value < currentCertainty
+                ? certaintyDiff / scalingFactor
+                : certaintyDiff * scalingFactor);
+        }
+
+        value = currentCertainty - finalCertaintyDiff;
+        Verse.Log.Message($"Scaled certainty to {value} diff change: {certaintyDiff}->{finalCertaintyDiff} with scaling factor {scalingFactor}");
+
+        // Return true to allow the original method to execute
+        return true;
+    }
+}
--- 1.4/Source/SocialWealth/Settings.cs
+++ 1.4/Source/SocialWealth/Settings.cs
@@ -6,14 +6,18 @@ namespace SocialWealth;
 public class Settings : ModSettings
 {
     //Use Mod.settings.setting to refer to this setting.
-    public bool setting = true;
+    public float neutralWealth = 100000f;
+    public float changeThreshold = 0.05f;
+    public string neutralWealthBuffer = "";
+    public string changeThresholdBuffer = "";
 
     public void DoWindowContents(Rect wrect)
     {
-        var options = new Listing_Standard();
+        Listing_Standard options = new();
         options.Begin(wrect);
         
-        options.CheckboxLabeled("Sample setting", ref setting);
+        options.TextFieldNumericLabeled("SocialWealth_Settings_NeutralWealth".Translate(), ref neutralWealth, ref neutralWealthBuffer, 0f, 10000000f);
+        options.TextFieldNumericLabeled("SocialWealth_Settings_ChangeThreshold", ref changeThreshold, ref changeThresholdBuffer, 0f, 1f);
         options.Gap();
 
         options.End();
@@ -21,6 +25,7 @@ public class Settings : ModSettings
     
     public override void ExposeData()
     {
-        Scribe_Values.Look(ref setting, "setting", true);
+        Scribe_Values.Look(ref neutralWealth, "neutralWealth", 100000);
+        Scribe_Values.Look(ref changeThreshold, "changeThreshold", 0.05f);
     }
 }
