6bda7a5221646f23c26c01a2ded3542a898b565c
Dexter Lowe <feldoh10@gmail.com>
2024-01-26 04:47:50 +0000

    Fix mote

--- 1.4/Source/SocialWealth/HarmonyPatches/ConversionRatePatch.cs
+++ 1.4/Source/SocialWealth/HarmonyPatches/ConversionRatePatch.cs
@@ -29,7 +29,8 @@ public static class ExtraLabelMouseAttachmentTranspiler
                     newCodes.Add(new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(ExtraLabelMouseAttachmentTranspiler), nameof(GetWealthAdjustedCertaintyReduction))));
                     break;
                 case true when t.opcode == OpCodes.Ret:
-                    newCodes.Add(new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(ExtraLabelMouseAttachmentTranspiler), nameof(AppendWealthAdjustedCertaintyReduction))));
+                    newCodes.Add(new CodeInstruction(OpCodes.Call,
+                        AccessTools.Method(typeof(ExtraLabelMouseAttachmentTranspiler), nameof(AppendWealthAdjustedCertaintyReduction))));
                     newCodes.Add(new CodeInstruction(OpCodes.Ret));
                     break;
                 default:
@@ -58,6 +59,48 @@ public static class ExtraLabelMouseAttachmentTranspiler
         return $"{s}\n -  " + "SocialWealth_WealthConversionFactorDesc".Translate() + ": " +
                GetCertaintyChangeMultiplier().ToStringPercent();
     }
+}
+
+
+[HarmonyPatch(typeof(Pawn_IdeoTracker), "IdeoConversionAttempt")]
+public static class IdeoConversionAttemptTranspiler
+{
+    public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+    {
+        List<CodeInstruction> codes = [..instructions];
+        List<CodeInstruction> newCodes = [];
+        int callCount = 0;
+        foreach (CodeInstruction t in codes)
+        {
+            if (t.Calls(AccessTools.Method(typeof(GenText), nameof(GenText.ToStringPercent), new[]
+                {
+                    typeof(float)
+                })))
+            {
+                callCount++;
+                switch (callCount)
+                {
+                    case 2:
+                        newCodes.Add(new CodeInstruction(OpCodes.Ldarg_0));
+                        newCodes.Add(new CodeInstruction(OpCodes.Call, AccessTools.PropertyGetter(typeof(Pawn_IdeoTracker), nameof(Pawn_IdeoTracker.Certainty))));
+                        newCodes.Add(new CodeInstruction(OpCodes.Call, AccessTools.Method(typeof(IdeoConversionAttemptTranspiler), nameof(GetWealthAdjustedCertaintyReducedToValue))));
+                        break;
+                }
+            }
+            newCodes.Add(t);
+        }
+
+        return newCodes.AsEnumerable();
+    }
+
+    public static float GetWealthAdjustedCertaintyReducedToValue(float newValue, float originalValue)
+    {
+        float diff = originalValue - newValue;
+        Verse.Log.Message($"{originalValue} - {newValue} = {diff} * {ExtraLabelMouseAttachmentTranspiler.GetCertaintyChangeMultiplier()}");
+        // We know this is an attempt to reduce certainty, and they must be not our ideo so we always want to multiply
+        return originalValue - diff * ExtraLabelMouseAttachmentTranspiler.GetCertaintyChangeMultiplier();
+    }
+
 }
 
 [HarmonyPatch(typeof(Pawn_IdeoTracker), nameof(Pawn_IdeoTracker.Certainty), MethodType.Setter)]
