6e6ae7c71bd677ae96117a078f1acb4dfab0c5d7
Dexter Lowe <feldoh10@gmail.com>
2024-01-28 00:15:54 +0000

    Thoughts

--- 1.4/Source/SocialWealth/Settings.cs
+++ 1.4/Source/SocialWealth/Settings.cs
@@ -6,13 +6,12 @@ namespace SocialWealth;
 
 public class Settings : ModSettings
 {
-    public const float _defaultNaturalWealth = 100000f;
+    public const float _defaultNaturalWealth = 200000f;
     public const float _defaultChangeThreshold = 0.05f;
 
     private float neutralWealth = -1f;
     private float changeThreshold = -1f;
     public string neutralWealthBuffer = "";
-    public string changeThresholdBuffer = "";
 
     public float NeutralWealth => neutralWealth <= 0f ? _defaultNaturalWealth : neutralWealth;
     public float ChangeThreshold => changeThreshold <= 0f ? _defaultChangeThreshold : changeThreshold;
@@ -23,23 +22,24 @@ public class Settings : ModSettings
     {
         Listing_Standard options = new();
         options.Begin(wrect);
-        
+        if (changeThreshold <= 0f) changeThreshold = _defaultChangeThreshold;
+        neutralWealthBuffer = NeutralWealth.ToString(CultureInfo.InvariantCulture);
         options.TextFieldNumericLabeled("SocialWealth_Settings_NeutralWealth".Translate(), ref neutralWealth, ref neutralWealthBuffer, 0f, 10000000f);
-        options.TextFieldNumericLabeled("SocialWealth_Settings_ChangeThreshold", ref changeThreshold, ref changeThresholdBuffer, 0f, 1f);
+        changeThreshold = options.SliderLabeled("SocialWealth_Settings_ChangeThreshold".Translate(changeThreshold.ToStringPercent("0.###")), changeThreshold, 0f, 1f,
+            tooltip: "SocialWealth_Settings_ChangeThreshold_Tip".Translate());
         options.Gap();
 
         options.End();
     }
-    
+
     public override void ExposeData()
     {
         Scribe_Values.Look(ref neutralWealth, "neutralWealth", _defaultNaturalWealth);
         Scribe_Values.Look(ref changeThreshold, "changeThreshold", _defaultChangeThreshold);
 
         if (Scribe.mode != LoadSaveMode.PostLoadInit) return;
-        neutralWealthBuffer = NeutralWealth.ToString(CultureInfo.InvariantCulture);
-        changeThresholdBuffer = ChangeThreshold.ToString(CultureInfo.InvariantCulture);
         if (neutralWealth <= 0f) neutralWealth = _defaultNaturalWealth;
         if (changeThreshold <= 0f) changeThreshold = _defaultChangeThreshold;
+        neutralWealthBuffer = NeutralWealth.ToString(CultureInfo.InvariantCulture);
     }
 }
---
+++ 1.4/Source/SocialWealth/ThoughtWorker_Precept_Wealth.cs
@@ -0,0 +1,37 @@
+﻿using RimWorld;
+using Verse;
+
+namespace SocialWealth;
+
+public class ThoughtWorker_Precept_Wealth : ThoughtWorker_Precept
+{
+    readonly float[] DefaultStageByWealthFactor =
+    [
+        0.2f,
+        0.5f,
+        1,
+        2,
+        4
+    ];
+
+    public virtual float[] StageByWealthFactor => DefaultStageByWealthFactor;
+
+    protected override ThoughtState ShouldHaveThought(Pawn p)
+    {
+        return !ModsConfig.IdeologyActive || !p.IsColonistPlayerControlled
+            ? ThoughtState.Inactive
+            : ThoughtState.ActiveAtStage(ThoughtStageIndex(p));
+    }
+
+    public virtual int ThoughtStageIndex(Pawn p)
+    {
+        float wealthFactor = SocialWealthMod.settings.WealthFactor(null);
+        for (int index = StageByWealthFactor.Length - 1; index >= 0; --index)
+        {
+            if (wealthFactor >= StageByWealthFactor[index])
+                return index + 2;
+        }
+
+        return 1;
+    }
+}
